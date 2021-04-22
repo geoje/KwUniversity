87475477915f1656b90209a22a635174ce376418
경호 양 <chchch1213@naver.com>
2021-04-22 14:15:45 +0900

    Add Chap07_Modaless

---
+++ ApplicationSystemPractice/ASP/Chap07_Modaless/FormMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Chap07_Modaless
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            FormColor fc = new FormColor();
+            fc.Owner = this;
+            fc.Changed += Fc_Changed;
+            fc.Show();
+        }
+
+        private void Fc_Changed(object sender, EventArgs e)
+            => BackColor = (sender as Form).BackColor;
+    }
+}
