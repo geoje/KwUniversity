5508e3ea82b5714bc75a576f66ddc2ccd8b0685f
Gyeongho Yang <chchch1213@naver.com>
2022-03-11 17:29:05 +0900

    모프2 과제1 진행중

--- MobileProgramming2/app/src/main/java/com/example/kwmp2/MainActivity.java
+++ MobileProgramming2/app/src/main/java/com/example/kwmp2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kwmp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnWeb).setOnClickListener(view ->
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"))));
+        findViewById(R.id.btnCall).setOnClickListener(view ->
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:911"))));
+        findViewById(R.id.btnGallery).setOnClickListener(view -> {});
+        findViewById(R.id.btnExit).setOnClickListener(view -> finish());
     }
 }
